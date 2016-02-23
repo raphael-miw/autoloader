9f7987e266813b6d707d49efecb011b210a3cb5d
raph <raph@e-frogg.com>
2016-02-23 04:04:22 +0100

    moving index to html folder

--- html/index.php
+++ html/index.php
@@ -1,7 +1,7 @@
 <?php
 
 /** @var \Composer\Autoload\ClassLoader $autoloader */
-$autoloader = require "vendor/autoload.php";
+$autoloader = require "../vendor/autoload.php";
 
 // ne peut être utilisé QUE si le classmap a été généré :
 // composer  dumpautoload -o
