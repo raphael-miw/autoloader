7fcfbaf7d9cb86725659c6d6f7fb53bd4e8bfa6a
raph <raph@e-frogg.com>
2016-02-23 03:52:13 +0100

    classmap autoritative

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
 
+/** @var \Composer\Autoload\ClassLoader $autoloader */
 $autoloader = require "vendor/autoload.php";
+
+// ne peut être utilisé QUE si le classmap a été généré :
+// composer  dumpautoload -o
+$autoloader -> setClassMapAuthoritative(true);
 
 $a = new \Html\Page\PageArticle();
 echo $a;  // __toString()
